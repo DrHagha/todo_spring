6e8d60a44420e3364fe506d6b86501e8911d9d9f
Jeon_minGyu <coby4313@gmail.com>
2023-04-05 11:37:36 +0900

    실행 확인

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,37 +1,40 @@
-package com.example.todo.security;
+package com.example.todo.config;
 
-import org.apache.catalina.filters.CorsFilter;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import lombok.extern.slf4j.Slf4j;
+import com.example.todo.security.JwtAuthenticationFilter;
 
+import lombok.extern.slf4j.Slf4j;
 
 @EnableWebSecurity
 @Slf4j
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
-	
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
 		http.cors()
-		.and()
-		.csrf()
-		.disable()
-		.httpBasic()
-		.disable()
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.authorizeRequests()
-		.antMatchers("/", "/auth/**").permitAll()
-		.anyRequest()
-		.authenticated();
-		
+				.and()
+				.csrf()
+				.disable()
+				.httpBasic()
+				.disable()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				.antMatchers("/", "/auth/**").permitAll()
+				.anyRequest()
+				.authenticated();
+
 		http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
 	}
 }
