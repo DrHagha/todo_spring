fd3b92d4561a4a878be9547d2085c02e6d5fda1d
Jeon_minGyu <coby4313@gmail.com>
2023-04-05 11:03:37 +0900

    강사님 코드

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,14 +32,14 @@ public class TodoController {
 	private TodoService service;
 	
 	@PostMapping
-	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+	public ResponseEntity<?> createTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 		try {
 			log.info("Log : createTodo entrance");
 			
 			TodoEntity entity = TodoDTO.toEntity(dto);
 			log.info("Log : dto => entity ok");
 			
-			entity.setUserId("admin-userid");
+			entity.setUserId(userId);
 			
 			Optional<TodoEntity> entities = service.create(entity);
 			log.info("Log : service.create ok");
@@ -59,9 +60,8 @@ public class TodoController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<?> retrieveTodoList(){
-		String temporaryUserId = "admin-userid";
-		List<TodoEntity> entities = service.retrieve(temporaryUserId);
+	public ResponseEntity<?> retrieveTodoList(@AuthenticationPrincipal String userId){
+		List<TodoEntity> entities = service.retrieve(userId);
 		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
 		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 		
@@ -69,11 +69,11 @@ public class TodoController {
 	}
 	
 	@GetMapping("/update")
-	public ResponseEntity<?> update(@RequestBody TodoDTO dto){
+	public ResponseEntity<?> update(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 		try {
 			TodoEntity entity = TodoDTO.toEntity(dto);
 			
-			entity.setUserId("admin-user");
+			entity.setUserId(userId);
 			
 			Optional<TodoEntity> entities = service.update(entity);
 			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
@@ -89,10 +89,10 @@ public class TodoController {
 	}
 	
 	@PutMapping
-	public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto){
+	public ResponseEntity<?> updateTodo(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 		try {
 			TodoEntity entity = TodoDTO.toEntity(dto);
-			entity.setUserId("admin-user");
+			entity.setUserId(userId);
 			Optional<TodoEntity> entities = service.updateTodo(entity);
 			
 			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
@@ -108,19 +108,20 @@ public class TodoController {
 	}
 	
 	@DeleteMapping
-	public ResponseEntity<?> delete(@RequestBody TodoDTO dto){
+	public ResponseEntity<?> delete(@AuthenticationPrincipal String userId, @RequestBody TodoDTO dto){
 		try {
-			List<String> message = new ArrayList<>();
-			String msg = service.delete(dto.getId());
-			message.add(msg);
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			entity.setUserId(userId);
+			List<TodoEntity> entities = service.delete(entity);
 			
-			ResponseDTO<String> response = ResponseDTO.<String>builder().data(message).build();
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 			
 			return ResponseEntity.ok().body(response);
 		}catch(Exception e) {
 			String error = e.getMessage();
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
-			
 			return ResponseEntity.badRequest().body(response);
 		}
 	}
--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -1,7 +1,6 @@
 package com.example.todo.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,57 +16,48 @@ public class TodoService {
 
 	@Autowired
 	private TodoRepository repository;
-	
-	public Optional<TodoEntity> create(final TodoEntity entity){
+
+	public List<TodoEntity> create(final TodoEntity entity) {
+		// Validations
 		validate(entity);
 		repository.save(entity);
-		return repository.findById(entity.getId());
+		// return repository.findById(entity.getId());
+		return repository.findByUserId(entity.getUserId());
 	}
-	
-	public List<TodoEntity> retrieve(final String userId){
+
+	public List<TodoEntity> retrieve(final String userId) {
 		return repository.findByUserId(userId);
 	}
-	
-	public Optional<TodoEntity> update(final TodoEntity entity){
+
+	public List<TodoEntity> update(final TodoEntity entity) {
+		// Validations
 		validate(entity);
-		if(repository.existsById(entity.getId())) {
+		if (repository.existsById(entity.getId())) {
 			repository.save(entity);
-		}else {
-			throw new RuntimeException("Unkown Todo id");
-		}
-		return repository.findById(entity.getId());
+		} else
+			throw new RuntimeException("Unknown id");
+
+		// return repository.findById(entity.getId());
+		return repository.findByUserId(entity.getUserId());
 	}
-	
-	public Optional<TodoEntity> updateTodo(final TodoEntity entity){
-		validate(entity);
-		
-		final Optional<TodoEntity> original = repository.findById(entity.getId());
-		
-		original.ifPresent(todo ->{
-			todo.setTitle(entity.getTitle());
-			todo.setDone(entity.isDone());
-			repository.save(todo);
-		});
-		return repository.findById(entity.getId());
-	}
-	
-	public String delete(final String id) {
-		if(repository.existsById(id)) {
-			repository.deleteById(id);
-		}else {
+
+	public List<TodoEntity> delete(final TodoEntity entity) {
+		if (repository.existsById(entity.getId()))
+			repository.deleteById(entity.getId());
+		else
 			throw new RuntimeException("id does not exist");
-		}
-		return "deleted!";
+
+		return repository.findByUserId(entity.getUserId());
 	}
-	
+
 	public void validate(final TodoEntity entity) {
-		if(entity == null) {
-			log.warn("Entity can't be null");
-			throw new RuntimeException("Entity can't be null");
+		if (entity == null) {
+			log.warn("Entity cannot be null.");
+			throw new RuntimeException("Entity cannot be null.");
 		}
-		if(entity.getUserId() == null) {
-			log.warn("Unknown user");
-			throw new RuntimeException("Unknown user");
+		if (entity.getUserId() == null) {
+			log.warn("Unknown user.");
+			throw new RuntimeException("Unknown user.");
 		}
 	}
 }
